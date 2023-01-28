d5b326e85aa713159495d7d143bee409dfbe57aa
Shivesh Tiwari <84664410+TheShiveshNetwork@users.noreply.github.com>
2023-01-28 11:36:08 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 
 import Router from 'next/router'
-import NProgress from 'nprogress'
 
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
@@ -10,17 +9,6 @@ import { Layout } from '../components'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    Router.events.on("routeChangeStart", (url) => {
-      NProgress.configure({ showSpinner: false, easing: 'ease', });
-      NProgress.start()
-    })
-
-    Router.events.on("routeChangeComplete", (url) => {
-      NProgress.done()
-    });
-  }, [Router])
-
   return (
     <>
       <Layout>
